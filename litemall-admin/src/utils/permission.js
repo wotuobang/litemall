685d2d3f600601fdc3a29100c3214d69cbbf674a
吴敏林 <minlin.wu@yat.com>
2020-01-10 13:54:25 +0800

    mall

---
+++ litemall-admin/src/utils/permission.js
@@ -0,0 +1,25 @@
+import store from '@/store'
+
+/**
+ * @param {Array} value
+ * @returns {Boolean}
+ * @example see @/views/permission/directive.vue
+ */
+export default function checkPermission(value) {
+  if (value && value instanceof Array && value.length > 0) {
+    const perms = store.getters && store.getters.perms
+    const permissions = value
+
+    const hasPermission = perms.some(perm => {
+      return permissions.includes(perm)
+    })
+
+    if (!hasPermission) {
+      return false
+    }
+    return true
+  } else {
+    console.error(`need perms! Like v-permission="['GET /aaa','POST /bbb']"`)
+    return false
+  }
+}
